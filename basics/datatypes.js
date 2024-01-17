29a320766a156151ef9ec6670b62bb6793ef57d0
Krishanu Mondal <137494766+krishanu717@users.noreply.github.com>
2024-01-17 05:42:19 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //treat all javascript  a newer version
+/*
+number => 2 pwer 53
+bigint => 
+string => ""
+boolen => true, false
+null => Standlone value
+undefined =>
+symbol=> unique
+
+object
+console.log(typeof""") for knowing the type 
+*/
+console.log(typeof Symbol)
+
