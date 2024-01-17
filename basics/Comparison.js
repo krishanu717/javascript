623aec47d30316b21d9af6bb20c39dc2e01a43d4
Krishanu Mondal <137494766+krishanu717@users.noreply.github.com>
2024-01-17 15:52:18 +0000

    comparison between datatypes

---
+++ basics/Comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+console.log(null == 0);
+//null means value is empty but 0 is a value
+console.log(undefined == 0);
+//undefined means it is a mot defined value but 0 is a defined value
+
+console.log("2" == 2);
+//js automatically converts the string datatypes to number datatypes
+
+// === strictly checks the datatypes and compare between them
+console.log("2" === 1);
+
