ae9741361d3a9e2a6e1c6703a665c428cd73cc0c
Krishanu Mondal <137494766+krishanu717@users.noreply.github.com>
2024-01-19 09:57:53 +0000

    datatypes summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+//there are two types of data in js =>
+
+//#7 types primitive Datatypes=> String, Number, BigiInt, Symbol, null, undefined, boolean
+
+//String
+//let name = "krish"
+
+//number
+let number = 546889
+
+//null
+let personLoggedIn = ""
+
+//boolean
+let city = true //or
+//let city = false
+
+//BigInt and if I comment out answer part it becomes undefined means this can not be defined
+let answer = 151580526031511551112n
+console.log(typeof answer);
+
+
+
+//#2 Non- primitive Datatypes=> object ,array ,function
+
+
+//array
+let heros = ["BhagatSingh" , "SubhasChadraBose", "KhudiramBose"]
+console.log(heros);
+
+//Object
+let myobj = {
+    Name :ram ,
+    age : 90,
+
+}
+
+// function using variables
+const myfunction = function(){
+console.log("hello world");
+}
