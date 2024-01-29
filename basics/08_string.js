1aaca314e486451dab9cea2a576775bf3c1c79ff
Krishanu Mondal <137494766+krishanu717@users.noreply.github.com>
2024-01-29 07:18:29 +0000

    String and their types

---
+++ basics/08_string.js
@@ -0,0 +1,21 @@
+const name = "krish"
+const roll = "10"
+// console.log(name + roll);// this is not modern way to write code 
+
+console.log(`hello my name is ${name} and my roll number is ${roll}`);
+// this is called string interpolation it gonna help when we are going to work in backend
+
+// another  way to writing string
+const gameName = new String('ApexLEgend')
+console.log(gameName);
+console.log(gameName.toUpperCase());
+//get string value in upper case
+console.log(gameName.charAt(5));
+//finding n ^th  number string value
+
+console.log(gameName.toLowerCase());
+//getting string value in lower case
+
+console.log(gameName.indexOf('g'));
+// getting number index in string value
+
