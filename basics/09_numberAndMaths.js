156dd74cd2b6c083b4187e1558cc517e1ffd603e
Krishanu Mondal <137494766+krishanu717@users.noreply.github.com>
2024-01-30 12:44:00 +0000

    numbers and math

---
+++ basics/09_numberAndMaths.js
@@ -0,0 +1,16 @@
+const score = 545
+console.log(score);
+
+const balance = new Number(10000)
+console.log(balance);
+// store value in object
+
+const querry = new Number(82288255)
+console.log(querry.toString());// convert  number to string 
+console.log(querry.toPrecision()); // get precise value
+console.log(querry.toFixed());// get round off value
+console.log(querry.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++//
+
+
